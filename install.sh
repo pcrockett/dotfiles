3a97a3fba444a47c483a0503abe60de25946feeb
Philip Crockett <contact@philcrockett.com>
2019-04-06 22:49:02 +0200

    Made paths a little more reliable in install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -Eeuo pipefail
 
+REPO_DIR=`dirname "$(readlink -f "$0")"`
+
 # Add soft links to files in this repo. If the destination file already exists,
 # the user will be prompted with an (expected) error message, but the show will
 # go on because of all the `|| true` bits at the end of the command.
-ln -sv ~/dotfiles/git/.gitconfig ~/.gitconfig || true
-ln -sv ~/dotfiles/bash/.profile ~/.profile || true
+ln -sv "$REPO_DIR/git/.gitconfig" ~/.gitconfig || true
+ln -sv "$REPO_DIR/bash/.profile" ~/.profile || true
