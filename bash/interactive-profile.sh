6bccb9bab833db743a7aa89354f05c430991990b
Philip Crockett <contact@philcrockett.com>
2019-06-15 23:19:00 +0200

    Better Bash setup

---
+++ bash/interactive-profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add customizations here
+
+# My own GPG key
+export GPG_MYSELF="B1AB796631CD7433EE213F9ACE9C332D8D86D48B"
+
+# Use this GPG key to encrypt something to my offline encryption key
+export OFFLINE_ENCRYPT_KEY="110054020B03B6FF!"
+
+# Leave this environment variable export here. It is used in the install script.
+export INTERACTIVE_PROFILE_LOADED=true
--- install.sh
+++ install.sh
@@ -9,4 +9,9 @@ REPO_DIR=`dirname "$(readlink -f "$0")"`
 # the user will be prompted with an (expected) error message, but the show will
 # go on because of all the `|| true` bits at the end of the command.
 ln -sv "$REPO_DIR/git/.gitconfig" ~/.gitconfig || true
-ln -sv "$REPO_DIR/bash/.profile" ~/.profile || true
+
+if [ ! -v INTERACTIVE_PROFILE_LOADED ]; then
+    echo "" >> ~/.bashrc
+    echo ". $REPO_DIR/bash/interactive-profile.sh >> /dev/null" >> ~/.bashrc
+    echo "Set up interactive profile in .bashrc. DO NOT run this install script again without restarting the shell first."
+fi
