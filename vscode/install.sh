858df9bed8d4c88ea6df1085e72f7af9d807f0ad
Philip Crockett <contact@philcrockett.com>
2019-06-28 07:44:11 +0200

    Added VSCodium bash install script

---
+++ vscode/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -Eeuo pipefail
+
+THISDIR=`dirname "$(readlink -f "$0")"`
+USERDIR=~/.config/VSCodium/User
+
+ln -sv "$THISDIR/settings.json" "$USERDIR/settings.json" || true
+ln -sv "$THISDIR/keybindings.json" "$USERDIR/keybindings.json" || true
+
+SNIPPETNAMES=`basename -a $THISDIR/snippets/*.json`
+for snippetname in $SNIPPETNAMES; do
+    ln -sv "$THISDIR/snippets/$snippetname" "$USERDIR/snippets/$snippetname" || true
+done
