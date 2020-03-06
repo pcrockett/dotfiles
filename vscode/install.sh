01380e98a7145775596a437db93f41c5e69171ab
Philip Crockett <contact@philcrockett.com>
2020-03-06 21:25:56 +0100

    Create VSCodium dir if it doesn't exist yet

--- vscode/install.sh
+++ vscode/install.sh
@@ -5,11 +5,14 @@ set -Eeuo pipefail
 
 THISDIR=$(dirname "$(readlink -f "$0")")
 USERDIR=~/.config/VSCodium/User
+SNIPPETSDIR="$USERDIR/snippets"
+
+mkdir --parents "$SNIPPETSDIR"
 
 ln -sv "$THISDIR/settings.json" "$USERDIR/settings.json" || true
 ln -sv "$THISDIR/keybindings.json" "$USERDIR/keybindings.json" || true
 
 SNIPPETNAMES=$(basename -a $THISDIR/snippets/*.json)
 for snippetname in $SNIPPETNAMES; do
-    ln -sv "$THISDIR/snippets/$snippetname" "$USERDIR/snippets/$snippetname" || true
+    ln -sv "$THISDIR/snippets/$snippetname" "$SNIPPETSDIR/$snippetname" || true
 done
