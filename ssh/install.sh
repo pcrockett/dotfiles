e2506008866d7bf870ae07f58a3f7e6df681cbca
Philip Crockett <contact@philcrockett.com>
2021-04-08 21:46:13 +0200

    Fix SSH config permissions, update script

--- ssh/install.sh
+++ ssh/install.sh
@@ -17,24 +17,25 @@ set -Eeuo pipefail
 [[ "${BASH_VERSINFO[0]}" -lt 4 ]] && echo "Bash >= 4 required" && exit 1
 
 # https://stackoverflow.com/a/1482133/138757
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+SCRIPT_DIR=$(dirname "$(readlink -f "${0}")")
 
 SSH_DIR=~/.ssh
-if [ ! -d "$SSH_DIR" ]; then
-    mkdir "$SSH_DIR"
+if [ ! -d "${SSH_DIR}" ]; then
+    mkdir "${SSH_DIR}"
 fi
 
-KEY_FILE="$SSH_DIR/id_ed25519"
-if [ ! -f "$KEY_FILE" ]; then
-    ssh-keygen -t ed25519 -a 100 -f "$KEY_FILE"
+KEY_FILE="${SSH_DIR}/id_ed25519"
+if [ ! -f "${KEY_FILE}" ]; then
+    ssh-keygen -t ed25519 -a 100 -f "${KEY_FILE}"
     echo ""
     echo "-----------------------------------------------------------"
     echo "New SSH key!"
     echo ""
-    cat "$KEY_FILE.pub"
+    cat "${KEY_FILE}.pub"
     echo ""
     echo "-----------------------------------------------------------"
     echo ""
 fi
 
-ln -sv "$SCRIPT_DIR/config" "$SSH_DIR/config" || true
+ln --symbolic --verbose "${SCRIPT_DIR}/config" "${SSH_DIR}/config" || true
+chmod go-rwx "${SCRIPT_DIR}/config"
